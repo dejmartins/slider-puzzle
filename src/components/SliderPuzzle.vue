8764d2dd7ed59b9e4dcefa215b65d4d5d18a47c3
Jonathan Martins <85448311+dejmartins@users.noreply.github.com>
2023-06-07 19:42:04 +0100

    SliderPuzzle component

--- src/components/SliderPuzzle.vue
+++ src/components/SliderPuzzle.vue
@@ -50,6 +50,60 @@
             elapsedTime(){
                 return moment.utc(this.elapsedDiff).format("HH:mm:ss")
             }
+        },
+        methods: {
+            swap(index){
+                if(!this.timer){
+                    return
+                }
+                if(this.indexesToSwap.length < 2){
+                    this.indexesToSwap.push(index)
+                }
+                if(this.indexesToSwap.length === 2){
+                    const [index1, index2] = this.indexesToSwap;
+                    [this.shuffledPuzzleArray[index1], this.shuffledPuzzleArray[index2]] = [
+                        this.shuffledPuzzleArray[index2],
+                        this.shuffledPuzzleArray[index1]
+                    ]
+                    this.indexesToSwap = []
+                }
+            },
+
+            start(){
+                this.resetTime();
+                this.shuffledPuzzleArray = [...correctPuzzleArray].sort(
+                    () => Math.random() - 0.5
+                )
+                this.indexesToSwap = []
+                this.timer = setInterval(() => {
+                    this.currentDateTime = new Date()
+                    if(this.isWinning){
+                        this.recordSpeedRecords()
+                        this.stop()
+                    }
+                }, 1000)
+            },
+
+            stop(){
+                this.resetTime()
+                clearInterval(this.timer)
+            },
+
+            resetTime(){
+                this.startDateTime = new Date()
+                this.currentDateTime = new Date()
+            },
+
+            recordSpeedRecords(){
+                let records = JSON.parse(localStorage.getItem("records")) || []
+                const { elapsedTime, elapsedDiff } = this
+                records.push({ elapsedTime, elapsedDiff })
+                const sortedRecords = records
+                    .sort((a, b) => a.elapsedDiff - b.elapsedDiff)
+                    .slice(0, 10)
+                const stringifiedRecords = JSON.stringify(sortedRecords)
+                localStorage.setItem("records", stringifiedRecords)
+            }
         }
     }
 </script>
